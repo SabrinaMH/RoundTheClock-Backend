c45eba8802a6bc7d4205ce56469c7623cb76c417
Sabrina <sabrina_privat@hotmail.com>
2015-01-18 21:37:26 +0100

    Working on the controllers

---
+++ Core/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using RoundTheClock.Core.Database;
+using RoundTheClock.Core.Repository;
+using RoundTheClock.Core.Utilities;
+using System.Web.Http;
+
+namespace RoundTheClock.Core.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        // Get all customers
+        public IHttpActionResult Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ Core/Controllers/TimeEntryController.cs
@@ -0,0 +1,19 @@
+using RoundTheClock.Core.Database;
+using RoundTheClock.Core.Model;
+using RoundTheClock.Core.Repository;
+using RoundTheClock.Core.Utilities;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RoundTheClock.Core.Controllers
+{
+    public class TimeEntryController : ApiController
+    {
+        public IHttpActionResult Post(IEnumerable<TimeEntry> entries)
+        {
+            var timeEntryRepository = new TimeEntryRepository(new DbConnection(ConnectionUtility.ConnectionString));
+            int noRows = timeEntryRepository.Insert(entries);
+            return Ok(noRows);
+        }
+    }
+}
