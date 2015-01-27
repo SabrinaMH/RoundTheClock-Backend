cd3d69baddc973b1bcfd04460979fa52c6f86adf
Sabrina <sabrina_privat@hotmail.com>
2015-01-27 17:44:13 +0100

    Unit test of Harvest API

---
+++ UnitTests/HarvestApiClientTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace RoundTheClock.UnitTests
+{
+    [TestFixture]
+    public class HarvestApiClientTests
+    {
+        //[Test]
+        //public void GetProjects()
+        //{
+        //    var client = new HarvestAPI();
+        //    client.GetProjects("energimidt", "smh@mjolner.dk", "myPassword");
+        //}
+    }
+}
